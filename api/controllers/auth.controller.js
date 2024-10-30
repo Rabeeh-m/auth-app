6bc255e51bf1abac5e258bbf42f4262e6915c0ce
Muhammed Rabeeh <rabeehm1999@gmail.com>
2024-10-31 01:41:22 +0530

    Create Sign in API route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken'
 
 
 export const signup = async (req, res, next) => {
@@ -14,3 +15,27 @@ export const signup = async (req, res, next) => {
         next(error);
     }
 };
+
+
+export const signin = async (req, res, next) => {
+    const {email, password} = req.body;
+
+    try {
+        const validUser = await User.findOne({ email});
+        if (!validUser) return next(errorHandler(404, 'User not found'));
+
+        const validPassword = bcrypt.compareSync(password, validUser.password);
+        if (!validPassword) return next(errorHandler(401, 'Invalid credentials'));
+        const {password: hashedPassword, ...rest} = validUser._doc;
+
+        const token = jwt.sign({id:validUser._id}, process.env.JWT_SECRET);
+        const expiryDate = new Date(Date.now() + 3600000);
+        res
+            .cookie('access_token', token, {httpOnly: true, expires: expiryDate})
+            .status(200)
+            .json(rest)
+
+    } catch (error) {
+        next(error)
+    }
+}
